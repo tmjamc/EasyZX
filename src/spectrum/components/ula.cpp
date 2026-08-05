93941cfb55ea875cb58e23ade6a3ee80464a9a4d
tmjamc <juanantonio.moreno.campos@gmail.com>
2026-08-05 21:33:19 +0200

    Changed default palette colors

--- src/spectrum/components/ula.cpp
+++ src/spectrum/components/ula.cpp
@@ -21,20 +21,20 @@ namespace ula
         constexpr uint32_t PALETTE[16] =
         {
             0x000000,   // black
-            0x0000c0,   // blue
-            0xc00000,   // red
-            0xc000c0,   // magenta
-            0x00c000,   // green
-            0x00c0c0,   // cyan
-            0xc0c000,   // yellow
-            0xc0c0c0,   // white
+            0x0100ce,   // blue
+            0xcf0100,   // red
+            0xcf01ce,   // magenta
+            0x00cf15,   // green
+            0x01cfcf,   // cyan
+            0xcfcf15,   // yellow
+            0xcfcfcf,   // white
             0x000000,   // bright black
-            0x0000ff,   // bright blue
-            0xff0000,   // bright red
-            0xff00ff,   // bright magenta
-            0x00ff00,   // bright green
-            0x00ffff,   // bright cyan
-            0xffff00,   // bright yellow
+            0x0200fd,   // bright blue
+            0xff0201,   // bright red
+            0xff02fd,   // bright magenta
+            0x00ff1c,   // bright green
+            0x02ffff,   // bright cyan
+            0xffff1d,   // bright yellow
             0xffffff    // bright white
         };
 
